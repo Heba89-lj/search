d5820c38fff0cac01dbdcce6109d15603f2b8fbf
Heba89-lj <h.jojo.loly.89@gmail.com>
2026-04-06 01:47:17 +0200

    Update incrementCounter.js

--- ahmed-search/api/incrementCounter.js
+++ ahmed-search/api/incrementCounter.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, updateDoc, increment, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain:"",
-  projectId: "",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
